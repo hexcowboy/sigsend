0226596e75a25753612ad0c093f1c3b693167df5
hexcowboy <accounts@cowboy.dev>
2023-06-14 14:30:18 -0700

    add social card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@hexcowboy" />
+        <meta name="twitter:creator" content="@hexcowboy" />
+        <meta name="twitter:title" content="SigSend" />
+        <meta
+          name="twitter:description"
+          content="Request crypto signatures from your friends, colleagues, and peers."
+        />
+        <meta
+          name="twitter:image"
+          content="https://r2.sigsend.xyz/twitter-card.webp"
+        />
+      </head>
       <body className="bg-white font-sans text-black dark:bg-black dark:text-white">
         {children}
         <Toaster />
